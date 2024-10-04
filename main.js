ecf5c6e1f0315ee7e3e539a56e5744e18d1d368d
Sarto_ <87348919+Sartokun@users.noreply.github.com>
2024-10-04 23:51:05 +0700

    Add files
    Add files: html,css,js
    Add folder: Assigment,Pictues

---
+++ main.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    const links = document.querySelectorAll('a:not(#header_bar a)');
+    links.forEach(function(link) {
+        link.setAttribute('target', '_blank');
+    });
+};
